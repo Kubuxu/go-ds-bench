abb6e9fa7ef84f21ac051f397e9f91eb554cc2e0
Jakub Sztandera <kubuxu@protonmail.ch>
2017-01-08 12:26:54 +0100

    limit batch size to 1GiB

--- prime.go
+++ prime.go
@@ -7,12 +7,17 @@ import (
 	ds "github.com/ipfs/go-datastore"
 )
 
+const primeMaxBatchSize = 1 << 30 // 1 GiB
+
 func PrimeDS(tb testing.TB, store ds.Batching, count, blockSize int) {
 	buf := make([]byte, blockSize)
+	maxBatchCount := primeMaxBatchSize / blockSize
+
 	b, err := store.Batch()
 	if err != nil {
 		tb.Fatal(err)
 	}
+
 	for i := 0; i < count; i++ {
 		_, err := rand.Read(buf)
 		if err != nil {
@@ -22,7 +27,19 @@ func PrimeDS(tb testing.TB, store ds.Batching, count, blockSize int) {
 		if err != nil {
 			tb.Fatal(err)
 		}
+
+		if i%maxBatchCount == maxBatchCount-1 {
+			err = b.Commit()
+			if err != nil {
+				tb.Fatal(err)
+			}
+			b, err = store.Batch()
+			if err != nil {
+				tb.Fatal(err)
+			}
+		}
 	}
+
 	err = b.Commit()
 	if err != nil {
 		tb.Fatal(err)
