9714f8c965f6ab21cfaa87876647bf1029ad5e72
Jakub Sztandera <kubuxu@protonmail.ch>
2017-03-13 15:40:23 +0100

    one more sync

--- add_batch.go
+++ add_batch.go
@@ -57,6 +57,7 @@ func BenchAddBatchSeriesOf(b *testing.B, newStore CandidateDatastore, opts []Ben
 			}
 			BenchAddBatchAt(b, store, opt, keys, bufs)
 			newStore.Destroy(store)
+			syscall.Sync()
 		})
 	}
 }
